b1345af8e471e61f3765a78745d14d8dfb89623e
Dhakalkeeran <keerandhakal@gmail.com>
2025-04-01 14:58:34 -0500

    Clean up survival plots file and make changes to dockefile, envs, and setup.sh

--- eclingo-docker/eclingo-benchmark/xperiments/survival_plots.py
+++ eclingo-docker/eclingo-benchmark/xperiments/survival_plots.py
@@ -1,18 +1,8 @@
-
-# coding: utf-8
-
-#get_ipython().magic(u'matplotlib notebook')
-from cProfile import label
-from optparse import Values
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from IPython.display import display, HTML, Markdown, display_jpeg
-import re
 import math
 from pathlib import Path
 import itertools
-
+import argparse
 
 
 pd.options.display.float_format = '{:,.1f}'.format
@@ -20,11 +10,7 @@ pd.options.display.float_format = '{:,.1f}'.format
 class Analyzer:
 
     def do_get_3df(self, df):
-
-        # Check 
-        print(df.isna().all(axis=1)[0])
-    
-        # Get index of first row will all nan values -> total tests
+        # Get index of first row with all nan values -> total tests
         try:
             nan  =  df.index[df.isna().all(axis=1)][0]
         except IndexError:
@@ -33,10 +19,6 @@ class Analyzer:
         # Get index for EP-ASP tests, as they have different DFs
         if nan == None:
             nan = len(df.isna().all(axis=1))
-            
-        old_t = 0
-        
-        df2 = df.filter(regex='script')
         
         # Append the times to construct tex file
         times=[0]
@@ -47,71 +29,40 @@ class Analyzer:
             elif cols[i] == "Times":
                 times.append(i)
                 
-
         run_df = df.iloc[1:nan, times]
-
         mini_df = run_df.iloc[:, 1:]
         total = mini_df.mean(axis=1)
         
         return list(total)
-
     
     # df is a list of DataFrames
     def get_3df(self, dfs):
         runs = {}
         for df in dfs:
-            print("get3df------",df[1])
+            print("solver name:", df[1])
             runs[df[1]] = self.do_get_3df(df[0])
         return runs
-    
-    
     
     def read_ods(self, _file):  # absolute path with .ods at the end
         import os
         os.system("soffice -env:UserInstallation=file:///$HOME/.libreoffice-headless/ --headless --convert-to xlsx " + _file)
-        xlsx = _file[:-3] + "xlsx"
+        xlsx = _file.rsplit('/', 1)[1][:-3] + "xlsx"
         instance_name = Path(_file).stem
-        
         return [pd.read_excel(xlsx), instance_name]
     
     def read_many_ods(self, files):
         return self.get_3df([self.read_ods(_file) for _file in files])
 
-    def get_series(self, times, time_range):
-
-        
-        counts = {}
-        times.sort()
-
-        j=0
-        count = 0
-
-
-        for t in time_range:
-
-            while j<len(times) and times[j]< t:
-                count+=1
-                j+=1
-
-            counts[t] = count
-
-        return counts
 
 def run(files, solver_names):
-
     a = Analyzer()
 
     # Obtain all running times for all battery of tests included.
     times = a.read_many_ods(files)
-    print("t--------",times)
-    print(len(times))
-    print(solver_names)
     all_times = list(itertools.chain.from_iterable(times.values()))
-
     all_times.sort()
-    print("How many:", len(all_times))
-    max_time = float(math.ceil(max(all_times))) # Get which is the maximum time for running.
     
+    max_time = float(math.ceil(max(all_times))) # Get which is the maximum time for running.
     max_time = min(max_time, 600)
     
     y_tick = {i*max_time/10 for i in range(1,11)}
@@ -141,35 +92,29 @@ def run(files, solver_names):
 '''
 
     colors = ["red", "blue", "green", "yellow", "black"]
-    if len(times) > len(colors):
-        print("Number of solvers more than colors. Please add more colors.")
-        exit(1)
+    while len(times) > len(colors):
+        colors = colors + colors
     
     for j, t in enumerate(times):
-
         ti = times[t]
-
         ti.sort()
-
         coordinates = []
         for i in range(len(ti)):
             s = "(" + str(i+1) + "," +  str(ti[i]) + ")"
             coordinates.append(s)
 
-
         coordinates_str = ''.join(coordinates)
 
         tex += f'''
-        \\addplot[
-        color={colors[j]},
-        mark=*,
-        ]
-        coordinates {{
-        {coordinates_str}
-        }};
-        \\addlegendentry{{ {solver_names[j]} }}
+\\addplot[
+color={colors[j]},
+mark=*,
+]
+coordinates {{
+{coordinates_str}
+}};
+\\addlegendentry{{ {solver_names[j]} }}
         '''
-
         
     tex += f'''
 \end{{axis}}
@@ -177,16 +122,26 @@ def run(files, solver_names):
 \end{{document}}
     '''
 
-    f = open("results.tex", "w")
+    write_path = "results.tex"
+    f = open(write_path, "w")
     f.write(tex)
     f.close()
     
-# TODO: Fix manual conversion of ods to xlsx without sudo privileges
-if __name__ == "__main__": 
-    solvers = ["eclingo", "eclingo-old"]
+    print("Tex file written at", write_path)
     
-    run(["eclingo.ods",
-         "eclingo-old.ods"], solvers)
+# TODO: Fix manual conversion of ods to xlsx without sudo privileges
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s","--solvers", nargs='+', help="name of solvers to use for plotting", required=True)
+    
+    solvers = parser.parse_args()._get_kwargs()[0][1]
+    
+    root_dir = Path(__file__).resolve().parent.parent
+    ods_file_paths = []
+    for solver in solvers:
+        ods_file_paths.append(str(root_dir/"running"/f"benchmark-tool-{solver}"/"experiments"/"results"/f"{solver}"/f"{solver}.ods"))
+    
+    run(ods_file_paths, solvers)
 
 
 
--- eclingo-docker/setup.sh
+++ eclingo-docker/setup.sh
@@ -4,6 +4,10 @@ eval "$(conda shell.bash hook)"
 
 conda activate ${env_name_1}
 ./run.sh ${solver_1} --max-instances=${max_instances} --benchmark=${benchmark}
+
 conda activate ${env_name_2}
 ./run.sh ${solver_2_name} --max-instances=${max_instances} --benchmark=${benchmark}
+
 python examine_output.py -s1 ${solver_1} -s2 ${solver_2} -s2n ${solver_2_name} > log.txt
+
+python xperiments/survival_plots.py -s ${solver_1} ${solver_2}
